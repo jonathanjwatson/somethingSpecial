7b6fddee8168aa94ece76940205d4ac90f0fe5bf
jwats287 <jwats287@users.noreply.github.com>
2019-07-27 09:54:29 -0400

    My work is done ehre

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
     console.log("Hello");
-    $("#content").append("<h2>My content will go here. I'm going to work on this for my project.</h2>");
+    $("#real-content").append("<h2>My content will go here. I'm going to work on this for my project.</h2>");
+
+    $("#header").append("<h1>Something Special</h1>");
+
 });
 
